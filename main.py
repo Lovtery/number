1c3094adb6aeaa14140f34c0b94cf87f6818ac45
Alexei Tsaralynga <alexeitsaralynga@gmail.com>
2024-10-15 20:44:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+number = int(input("введите целое число "))
+if number > 0:
+    print("число положительное")
+if number < 0:
+    print("число отрицательное ")
+if number == 0:
+    print("число равно нулю ")
+if number %2 == 0:
+    print("число четное")
+else:
+    print("число нечетное")
